96d926b07b4f9c96228b0272bc7a291f4f7e3cc8
Elymne <sacha.djurdjevic@gmail.com>
2025-10-04 04:34:27 +0200

    fix(ble): remove BLE solution

--- lib/infrastructure/gateways/street_path/flutter_foreground_task.dart
+++ lib/infrastructure/gateways/street_path/flutter_foreground_task.dart
@@ -1,7 +1,6 @@
 // The callback function should always be a top-level or static function.
 import 'package:flutter/foundation.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
-import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
 // * Obligé d'utiliser une fonction statique pour le callback lors de démarrage du service.
 @pragma('vm:entry-point')
@@ -12,8 +11,6 @@ void streetPathTaskHandlerCallback() {
 /// Classe utilisé par l'implémentation de l'interface StreetPathGateway : StreetPathGatewayImpl
 /// Libs : flutter_reactive_ble, flutter_foreground_task
 class StreetPathTaskHandler extends TaskHandler {
-  final FlutterReactiveBle _ble = FlutterReactiveBle();
-
   /// Cette appel de fonction ne sert pour l'instant juste à log et à tester la librairie.
   /// todo : Check si il y a des choses à regarder.
   @override
@@ -24,38 +21,7 @@ class StreetPathTaskHandler extends TaskHandler {
   /// ! Cette fonction de callback sera appellé à une intervale de temps x défini dans la classe StreetPathGatewayImpl (fonction d'initialisation).
   /// Chaque x intervale de temps : Récupération de tous les devices détecté à proximité (class DiscoveredDevice).
   @override
-  void onRepeatEvent(DateTime timestamp) {
-    _ble.scanForDevices(withServices: [], scanMode: ScanMode.lowPower).listen((device) {
-      if (kDebugMode) print('Device found: ${device.name} (${device.id})');
-    });
-  }
-
-  /// Permet la connexion à l'appareil rentré en paramètre.
-  /// todo : checker si l'appareil à l'app, si elle emet, si elle veut transmettre et veut recevoir.
-  Future connectToDevice(DiscoveredDevice device) async {
-    late QualifiedCharacteristic characteristic;
-
-    // 1. Connect.
-    final connection = _ble.connectToDevice(id: device.id);
-
-    connection.listen((connectionState) async {
-      // 2. Définir la caractéristique (tu dois connaître les UUID)
-      const serviceUuid = "0000180f-0000-1000-8000-00805f9b34fb";
-      const characteristicUuid = "00002a19-0000-1000-8000-00805f9b34fb";
-
-      characteristic = QualifiedCharacteristic(
-        serviceId: serviceUuid as Uuid,
-        characteristicId: characteristicUuid as Uuid,
-        deviceId: device.id,
-      );
-
-      // 3. Envoyer des données (ici on envoie un tableau d’octets)
-      final payload = [0x01, 0x02, 0x03]; // tes données
-      await _ble.writeCharacteristicWithResponse(characteristic, value: payload);
-
-      print("📡 Data sent: $payload");
-    });
-  }
+  void onRepeatEvent(DateTime timestamp) {}
 
   // ----------------------------------
   // -- NON USED
